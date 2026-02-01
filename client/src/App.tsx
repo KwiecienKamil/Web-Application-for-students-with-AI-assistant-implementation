54b18ecb2f83b0a43706c1c9cdd59e6fd1230489
Kamil <kamilkwiecien9@gmail.com>
2026-02-01 21:19:46 +0100

    Setup second database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import type { RootState } from "./store";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
