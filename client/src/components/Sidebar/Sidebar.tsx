6fe8fc11593442fcbfcc7b7e8687993666eff950
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2026-06-27 10:45:22 +0200

    fix: filter navLinks and show only relative ones to premium/not premium users

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -11,79 +11,82 @@ import { BiLogOutCircle } from "react-icons/bi";
 import type { User } from "../../types/UserProps";
 
 type SidebarProps = {
-	user: User | null;
+  user: User | null;
 };
 
 const Sidebar = ({ user }: SidebarProps) => {
-	const navigate = useNavigate();
-	const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
-	const signOut = async () => {
-		await supabase.auth.signOut();
-		navigate("/login");
-	};
+  const signOut = async () => {
+    await supabase.auth.signOut();
+    navigate("/login");
+  };
 
-	// Some users have their full name, pick only first name
-	const displayOnlyFirstName = user?.name?.split(" ")[0];
+  // Some users have their full name, pick only first name
+  const displayOnlyFirstName = user?.name?.split(" ")[0];
 
-	return (
-		<>
-			<div id="sidebar-menu-button">
-				<FiMenu onClick={() => setIsOpen((prev) => !prev)} />
-			</div>
-			<div id="sidebar-wrapper" className={isOpen ? "open" : ""}>
-				<RiArrowLeftBoxFill
-					id="sidebar-menu-close-button"
-					onClick={() => setIsOpen((prev) => !prev)}
-				/>
-				<img src={logo} id="sidebar-logo" alt="Ogarnijto.org logo" />
-				<div>
-					<div id="sidebar-user-info">
-						<img
-							src={user?.picture ? user.picture : defaultUserImage}
-							className="sidebar-user-image"
-							alt="Zdjęcie użytkownika"
-							referrerPolicy="no-referrer"
-						/>
-						<div>
-							<h1>{`Cześć${user?.name ? "," : ""} ${user?.name ? displayOnlyFirstName : ""} !`}</h1>
-							{user?.isPremium ? (
-								<p>
-									Konto <span id="premium">Premium</span>
-								</p>
-							) : (
-								<p>Konto Zwykłe</p>
-							)}
-						</div>
-					</div>
-					<nav>
-						<ul>
-							{navLinks.map((navLink) => {
-								const Icon = navLink.icon;
-								return (
-									<li key={navLink.id}>
-										<NavLink
-											to={navLink.link}
-											className={({ isActive }) => (isActive ? "active" : "")}
-										>
-											<Icon />
-											{navLink.title}
-										</NavLink>
-									</li>
-								);
-							})}
-							<li>
-								<button type="button" onClick={signOut}>
-									<BiLogOutCircle />
-									Wyloguj się
-								</button>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div id="sidebar-menu-button">
+        <FiMenu onClick={() => setIsOpen((prev) => !prev)} />
+      </div>
+      <div id="sidebar-wrapper" className={isOpen ? "open" : ""}>
+        <RiArrowLeftBoxFill
+          id="sidebar-menu-close-button"
+          onClick={() => setIsOpen((prev) => !prev)}
+        />
+        <img src={logo} id="sidebar-logo" alt="Ogarnijto.org logo" />
+        <div>
+          <div id="sidebar-user-info">
+            <img
+              src={user?.picture ? user.picture : defaultUserImage}
+              className="sidebar-user-image"
+              alt="Zdjęcie użytkownika"
+              referrerPolicy="no-referrer"
+            />
+            <div>
+              <h1>{`Cześć${user?.name ? "," : ""} ${user?.name ? displayOnlyFirstName : ""} !`}</h1>
+              {user?.isPremium ? (
+                <p>
+                  Konto <span id="premium">Premium</span>
+                </p>
+              ) : (
+                <p>Konto Zwykłe</p>
+              )}
+            </div>
+          </div>
+          <nav>
+            <ul>
+              {navLinks
+                .filter((link) => !(user?.isPremium && link.hideForPremium))
+                .map((navLink) => {
+                  const Icon = navLink.icon;
+
+                  return (
+                    <li key={navLink.id}>
+                      <NavLink
+                        to={navLink.link}
+                        className={({ isActive }) => (isActive ? "active" : "")}
+                      >
+                        <Icon />
+                        {navLink.title}
+                      </NavLink>
+                    </li>
+                  );
+                })}
+              <li>
+                <button type="button" onClick={signOut}>
+                  <BiLogOutCircle />
+                  Wyloguj się
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
